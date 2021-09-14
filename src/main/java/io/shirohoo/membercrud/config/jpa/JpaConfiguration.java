c1dd1d3363a7521dc38e9411efb6fdc268bdbd48
shirohoo <71188307+shirohoo@users.noreply.github.com>
2021-09-14 09:24:38 +0900

    Update JpaConfiguration.java

--- src/main/java/io/shirohoo/membercrud/config/jpa/JpaConfiguration.java
+++ src/main/java/io/shirohoo/membercrud/config/jpa/JpaConfiguration.java
@@ -2,6 +2,7 @@ package io.shirohoo.membercrud.config.jpa;
 
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
 @EnableJpaAuditing
 public class JpaConfiguration {
 
